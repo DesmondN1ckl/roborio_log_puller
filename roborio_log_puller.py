7714d8e43d31f14b9b1ce2783414cd843931c821
Desmond Nickl <desmondnickl@gmail.com>
2026-04-19 14:48:58 -0500

    Update main logic and connection functions

--- roborio_log_puller.py
+++ roborio_log_puller.py
@@ -2,39 +2,52 @@ import os
 import paramiko
 import socket
 
-daemon_mode = False
-current_dir = os.getcwd()
-ssh = paramiko.SSHClient()
+daemon_mode: bool = False
+current_dir: str = os.getcwd()
+ssh_default_user: str = "lvuser"
+ssh_admin_user: str = "admin"
+use_admin: bool = False
 
-team_number = 112
-log_dir = "match_logs"
-roborio_mdns = f"roboRIO-{team_number}-frc.local"
-fallback_roborio_ip = "10.1.12.2"
+team_number: int = 112
+fallback_roborio_ip: str = "10.1.12.2"
+log_dir: str = "match_logs" # in cwd
+roborio_mdns: str = f"roboRIO-{team_number}-frc.local"
 
-
-def check_logs_dir():
+def check_logs_dir() -> None:
     os.makedirs(log_dir, exist_ok=True)
 
-def connect():
-    ssh.connect(fallback_roborio_ip, username="lvuser", password="")
+def ssh_connect(address: str, username: str, password: str) -> paramiko.SSHClient:
+    ssh_client: paramiko.SSHClient = paramiko.SSHClient()
+    ssh_client.connect(address, username=username, password=password, timeout=3)
+    return ssh_client
 
-def run_command(cmd):
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd)
+def ssh_run_command(ssh_client: paramiko.SSHClient, cmd: str) -> str:
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh_client.exec_command(cmd)
     output = ssh_stdout.read().decode() # wait for command to return
 
     return output
 
-def disconnect():
-    ssh.close()
+def ssh_disconnect(ssh_client: paramiko.SSHClient) -> None:
+    ssh_client.close()
 
-def resolve_dns():
+def resolve_roborio() -> str:
     try:
         response = socket.gethostbyname(roborio_mdns)
-    except socket.gaierror as e:
+    except socket.gaierror:
         response = fallback_roborio_ip
 
     return response
 
 if (__name__ == "__main__"):
     check_logs_dir()
-    
+    addr = resolve_roborio()
+    user: str = ssh_default_user
+
+    if use_admin:
+        user = ssh_admin_user
+
+    ssh_client = ssh_connect(addr, user, "")
+
+    # future logic
+
+    ssh_disconnect(ssh_client)
